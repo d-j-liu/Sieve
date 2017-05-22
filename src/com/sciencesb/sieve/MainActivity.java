2918c47cd699d3976c6bd0cc55c9b6a68a9aad19
d-j-liu <d_j_liu@hotmail.com>
2017-05-21 17:17:08 -0700

    Integrate the sieve algorithm.

--- src/com/sciencesb/sieve/MainActivity.java
+++ src/com/sciencesb/sieve/MainActivity.java
@@ -12,16 +12,19 @@ import android.widget.GridView;
 
 public class MainActivity extends Activity {
 
+    // size of the grid
     public static final int minCol = 3;
     public static final int maxCol = 7;
+    private int _col = ( minCol + maxCol ) / 2;
 
+    // the grid view and its dynamic content
     private ArrayList<String> _nums = new ArrayList<String>();
     private ArrayAdapter<String> _adapter = null;
     private GridView _grid = null;
-    private int _col = ( minCol + maxCol ) / 2;
 
+    // the engine
     private Timer _timer = null;
-    private int _current = 0;
+    private Sieve _sieve = null;
 
     public static void log( String msg ) {
         android.util.Log.println( 4, "Sieve", msg );
@@ -30,7 +33,8 @@ public class MainActivity extends Activity {
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        _adapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, _nums );
+        _adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, _nums );
         _grid = (GridView)findViewById( R.id.gridView );
         _grid.setAdapter( _adapter );
         _init();
@@ -38,10 +42,12 @@ public class MainActivity extends Activity {
 
     private void _init() {
         final int row = ( _col * 16 + 8 ) / 9;
-        final int max = _col * row;
+        final int count = _col * row;
+        final int max = count + 1;
+        _sieve = new Sieve( max );
         _grid.setNumColumns( _col );
         _nums.clear();
-        for( int i = 0; i < max; ++i ) {
+        for( int i = 2; i <= max; ++i ) {
             _nums.add( "" + i );
         }
         _adapter.notifyDataSetChanged();
@@ -65,15 +71,24 @@ public class MainActivity extends Activity {
 
     private class Advancer extends TimerTask {
         public void run() {
+            // action on UI must be scheduled on its own thread
             _grid.post( new Updater() );
         }
     }
 
     public class Updater implements Runnable {
         public void run() {
-            final View item = _grid.getChildAt( 0 );
-            if( item == null ) log( "Grid item not found." );
-            else item.setBackgroundColor( ++_current % 2 == 0 ? 0xFF00FF00 : 0xFF0000FF );
+            final int n = _sieve.advance();
+            if( n == 0 ) { // finish
+                if( _timer != null ) _timer.cancel();
+                _timer = null;
+                return;
+            }
+            // change background color based on prime vs. composite
+            final int index = n > 0 ? n : -n;
+            final View item = _grid.getChildAt( index - 2 );
+            if( item == null ) log( "Missing grid item for " + n );
+            else item.setBackgroundColor( n > 0 ? 0xFF00FF00 : 0xFFFF0000 );
         }
     }
 }
